a2ad8aac3e4a2852050aa28155c6eba1f4ebd46a
Luis Chang <luis.chang@iugo.com.uy>
2022-04-02 11:04:18 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/SecurityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Semana02_UESAN_FirstMVC.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Semana02_UESAN_FirstMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
